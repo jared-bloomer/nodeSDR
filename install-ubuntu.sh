3dbb0ee3e3521f12463def81d4431e5a4d1c15b8
jared-bloomer <me@jaredbloomer.com>
2023-10-02 00:47:06 +0000

    fixing if condidtional

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -2,16 +2,17 @@
 
 packages="librtlsdr-dev librtlsdr0 libusb-1.0-0-dev libusb-1.0-doc"
 
-if "$UID" != "0"; then
-	echo "ERROR: You must be root or use sudo to run this script!"
-	exit 1
-else
-	main
-fi
-
 main () {
 	apt update
 	apt install -y $packages
 
 	exit 0
 }
+
+if [ $UID -ne 0 ]; then
+	echo "ERROR: You must be root or use sudo to run this script!"
+	exit 1
+else
+	main
+fi
+

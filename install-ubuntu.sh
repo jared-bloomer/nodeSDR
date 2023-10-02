a591188247660818a743bd7d7428b5c39606d111
Jared-Bloomer <me@jaredbloomer.com>
2023-10-01 21:01:17 -0400

    adding Node Steps

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
 
-packages="librtlsdr-dev librtlsdr0 libusb-1.0-0-dev libusb-1.0-doc"
+packages="librtlsdr-dev librtlsdr0 libusb-1.0-0-dev libusb-1.0-doc git wget tar"
+
+installNVM () {
+	echo "Install Node Version Manager (nvm)"
+	wget https://github.com/nvm-sh/nvm/archive/refs/tags/v0.39.5.tar.gz
+	tar xf v0.39.5.tar.gz
+	cd nvm-0.39.5
+	bash install.sh
+	cd ..
+}
 
 main () {
 	apt update
 	apt install -y $packages
-
+	installNVM
+	cd server
+	echo "Installing Node v18.18.0"
+	nvm install v18.18.0
+	echo "Install nodejs packages needed for server"
+	npm i
+	echo "Setting up Server Database"
+	npm exec knex init
+	npm exec knex migrate:up
+	
 	exit 0
 }
 

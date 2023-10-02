f6e416d1afac95219c9d6617b5654f5a3e22a03f
jared-bloomer <me@jaredbloomer.com>
2023-10-02 00:41:24 +0000

    inital commit

---
+++ install-ubuntu.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+packages="librtlsdr-dev librtlsdr0 libusb-1.0-0-dev libusb-1.0-doc"
+
+if "$UID" != "0"; then
+	echo "ERROR: You must be root or use sudo to run this script!"
+	exit 1
+fi
+
+apt update
+apt install -y $packages
+
+exit 0

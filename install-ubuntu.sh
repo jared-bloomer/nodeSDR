5f4eb21f0703c2533f69f765010869b62de422a8
jared-bloomer <me@jaredbloomer.com>
2023-10-02 00:43:53 +0000

    using main function

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -5,9 +5,13 @@ packages="librtlsdr-dev librtlsdr0 libusb-1.0-0-dev libusb-1.0-doc"
 if "$UID" != "0"; then
 	echo "ERROR: You must be root or use sudo to run this script!"
 	exit 1
+else
+	main
 fi
 
-apt update
-apt install -y $packages
+main () {
+	apt update
+	apt install -y $packages
 
-exit 0
+	exit 0
+}
